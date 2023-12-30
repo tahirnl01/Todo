d81505ba149c59a38edd9268e757735a1725ff3e
tahirnl01 <151435166+tahirnl01@users.noreply.github.com>
2023-12-30 19:29:48 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React ,{useState} from 'react';
+import './App.css';
+
+function App() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+  function addItem(){
+    if(!newItem){
+      alert("enter an item");
+      return;
+    }
+    const item={
+      id: Math.floor(Math.random()*1000),
+      value: newItem
+    }
+    setItems(oldItems => [...oldItems,item])
+    setNewItem("");
+  }
+  function deleteItem(id){
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray);
+
+  }
+
+ 
+  return (
+   <div className="App">
+    <h1>yapılacaklar Listesi</h1>
+    <input
+    type="text"
+    placeholder="yeni görev ekle..." 
+    value={newItem}
+    onChange={e=>setNewItem(e.target.value)}
+    />
+
+    <button onClick={()=> addItem()}>Ekle</button>
+
+    <ul>
+      {items.map(item=>{
+        return(
+          <li key={item.id}>{item.value} <button onClick={()=>deleteItem(item.id)}>X</button></li>
+        )
+
+      })}
+    </ul>
+    
+   </div>
+  )
+}
+
+export default App;
